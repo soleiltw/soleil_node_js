96625ec4938b4b8f4885f17bc2fb2396c3f652a9
sylvia@soleil.tw <sylvia8431@soleil.tw>
2015-08-26 11:12:46 +0800

    add another server to do cronjob

---
+++ student_tutor_API/cronjob.js
@@ -0,0 +1,29 @@
+var StudentModel = require('./models/student');
+var CronJob = require('cron').CronJob;
+var job = new CronJob('00 55 18 * * 0-6', function() {
+  /*
+   * Runs everyday
+   * at 18:55:00 AM. 
+   */
+    
+    StudentModel.find({quota: 0 },function (error, student) {
+    
+            if (!error) {
+                for (var i = student.length - 1; i >= 0; i--) {
+                  student[i].quota += 1;
+                  student[i].save(function(err) {
+                  if (err) { return next(err); }
+                  });
+                }; 
+            } else {
+              //TODO: return page with errors
+              return console.log(error);
+            }  
+    });  
+
+  }, function () {
+    /* This function is executed when the job stops */
+  },
+  true, /* Start the job right now */
+  'Asia/Taipei' /* Time zone of this job. */
+);
---
+++ student_tutor_API/server6.js
@@ -0,0 +1,29 @@
+/**
+ * Created by sylvia on 2015/8/26.
+ */
+
+var express = require('express')
+  , http = require('http')
+  , mongoose = require('mongoose')
+  , json = require('json')
+  , path = require('path')
+  , favicon = require('serve-favicon')
+  , bodyParser = require('body-parser');
+
+var config = require('./config');
+var middleHandler = require("./middleHandler");
+
+var app = express();
+
+mongoose.connect(config.database);
+
+app.set('port', process.env.PORT || 3001);
+app.set('superSecret', config.secret);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false })); 
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
